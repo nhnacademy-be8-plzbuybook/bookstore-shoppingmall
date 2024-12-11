057bef1ca45fbaa5ea35d56023cb6890146038e4
guksy <todus723@gmail.com>
2024-12-11 17:52:15 +0900

    Auth, MemberAuth 추가했습니다

---
+++ src/main/java/com/nhnacademy/book/member/domain/Auth.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.book.member.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Auth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long authId;
+
+    @Column(nullable = false, length = 50)
+    private String authName;
+}
