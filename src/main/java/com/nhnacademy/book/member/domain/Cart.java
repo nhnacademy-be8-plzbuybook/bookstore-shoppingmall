a49d9d460fb94a9092b1ac4239e5cd6e1022dcc9
guksy <todus723@gmail.com>
2024-12-11 16:57:49 +0900

    cart 추가했습니다

--- src/main/java/com/nhnacademy/book/member/domain/Cart.java
+++ src/main/java/com/nhnacademy/book/member/domain/Cart.java
@@ -6,27 +6,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class MemberCertification {
+public class Cart {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
-    private Long memberAuthId;
+    private Long cartId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "mc_member_id", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "c_member_id", nullable = false)
     private Member member;
-
-    @Column
-    private LocalDateTime lastLogin;
-
-    @Column(nullable = false, columnDefinition = "일반")
-    private String certificationMethod;
 }
