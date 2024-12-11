a6cebe7b3994579d24973e288150d11c1d46a656
yunjiho <yoonwlgh12@gmail.com>
2024-12-11 15:15:54 +0900

    Review 매핑 완료했습니다

---
+++ src/main/java/com/nhnacademy/book/domain/Review.java
@@ -0,0 +1,45 @@
+package com.nhnacademy.book.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private long reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "rm_id", nullable = false)
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "rop_id", nullable = false)
+    private OrderProduct orderProduct;
+
+    //평가 점수
+    private Integer score;
+
+    //리뷰 내용
+    private String content;
+
+    //리뷰 작성 날짜
+    @Column(name = "write_date")
+    private LocalDateTime writeDate;
+
+    //리뷰 수정 날짜
+    @Column(name = "modify_date")
+    private LocalDateTime modifiedDate;
+
+}
