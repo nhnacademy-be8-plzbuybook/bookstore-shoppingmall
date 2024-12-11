a652104bf8f33c1fec2944390e5b78feb57c978e
choegwanghyeog <fkqlaus@naver.com>
2024-12-11 09:22:02 +0900

    쇼핑몰 서버 인증서버랑 통신 테스트

---
+++ src/main/java/com/nhnacademy/book/controller/AuthClient.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.book.controller;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "authentication")
+public interface AuthClient {
+
+    @GetMapping("/hello")
+    String hello();
+}
