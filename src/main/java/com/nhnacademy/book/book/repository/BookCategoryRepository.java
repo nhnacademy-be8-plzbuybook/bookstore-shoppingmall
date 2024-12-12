ae2ec57ecc17495073d7915552707d4ec3853a48
choegwanghyeog <fkqlaus@naver.com>
2024-12-12 09:12:50 +0900

    book repo 생성

--- src/main/java/com/nhnacademy/book/book/repository/BookCategoryRepository.java
+++ src/main/java/com/nhnacademy/book/book/repository/BookCategoryRepository.java
@@ -1,9 +1,9 @@
 package com.nhnacademy.book.book.repository;
 
+import com.nhnacademy.book.book.entity.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface BookAuthor extends JpaRepository<BookAuthor, Long> {
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 }
