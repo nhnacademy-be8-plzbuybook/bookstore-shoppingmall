897a932dd940d7785e55016f2a13b24fe86387f0
yunjiho <yoonwlgh12@gmail.com>
2024-12-12 13:17:07 +0900

    회원을 추가하는 service를 구현했습니다.

---
+++ src/main/java/com/nhnacademy/book/member/domain/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.book.member.domain.service;
+
+import com.nhnacademy.book.member.domain.Member;
+import com.nhnacademy.book.member.domain.dto.MemberCreateRequestDto;
+
+import java.time.LocalDate;
+
+public interface MemberService {
+    Member save(MemberCreateRequestDto memberCreateRequestDto);
+}
