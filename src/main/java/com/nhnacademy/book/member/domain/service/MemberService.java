98abd32a909c5d73f62d86d50607ee1e483152cc
yunjiho <yoonwlgh12@gmail.com>
2024-12-13 09:32:19 +0900

    email로 회원 조회할때 필요한 repository, service 구현

--- src/main/java/com/nhnacademy/book/member/domain/service/MemberService.java
+++ src/main/java/com/nhnacademy/book/member/domain/service/MemberService.java
@@ -3,11 +3,7 @@ package com.nhnacademy.book.member.domain.service;
 import com.nhnacademy.book.member.domain.Member;
 import com.nhnacademy.book.member.domain.MemberGrade;
 import com.nhnacademy.book.member.domain.MemberStatus;
-import com.nhnacademy.book.member.domain.dto.MemberCreateRequestDto;
-import com.nhnacademy.book.member.domain.dto.MemberGradeCreateRequestDto;
-import com.nhnacademy.book.member.domain.dto.MemberModifyRequestDto;
-import com.nhnacademy.book.member.domain.dto.MemberStatusCreateRequestDto;
-
+import com.nhnacademy.book.member.domain.dto.*;
 
 
 public interface MemberService {
@@ -17,4 +13,5 @@ public interface MemberService {
     MemberStatus save(MemberStatusCreateRequestDto memberStatusCreateRequestDto);
     MemberGrade findByMemberGradeId(Long id);
     MemberStatus findByMemberStatusId(Long id);
+    MemberEmailResponseDto getMemberByEmail(String email);
 }
