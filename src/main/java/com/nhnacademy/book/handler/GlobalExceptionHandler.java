42466983cce93730c026ac0f42099dd15674472c
yunjiho <yoonwlgh12@gmail.com>
2024-12-14 21:15:53 +0900

    중복된 상태를 저장하려할때 예외를 전역적으로 받아서 처리하게 했습니다 + 예외에 대한 주석들 달아줌

--- src/main/java/com/nhnacademy/book/handler/GlobalExceptionHandler.java
+++ src/main/java/com/nhnacademy/book/handler/GlobalExceptionHandler.java
@@ -8,39 +8,51 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    //중복 이메일
     @ExceptionHandler(DuplicateEmailException.class)
     public ResponseEntity<String> handleDuplicateEmailException(DuplicateEmailException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
+    //회원 등급을 찾을 수 없는 예외 처리
     @ExceptionHandler(MemberGradeNotFoundException.class)
     public ResponseEntity<String> handleMemberGradeNotFoundException(MemberGradeNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    //회원 상태를 찾을 수 없는 예외 처리
     @ExceptionHandler(MemberStatusNotFoundException.class)
     public ResponseEntity<String> handleMemberStatusNotFoundException(MemberStatusNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    //기본 등급을 찾을 수 없는 예외 처리
     @ExceptionHandler(DefaultMemberGradeNotFoundException.class)
     public ResponseEntity<String> handleDefaultMemberGradeNotFoundException(DefaultMemberGradeNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    //기본 상태를 찾을 수 없는 예외 처리
     @ExceptionHandler(DefaultStatusGradeNotfoundException.class)
     public ResponseEntity<String> handleDefaultStatusGradeNotFoundException(DefaultStatusGradeNotfoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    //이메일로 특정 회원을 조회 할 때 찾을 수 없는 예외 처리
     @ExceptionHandler(MemberEmailNotFoundException.class)
     public ResponseEntity<String> handleMemberEmailNotFoundException(MemberEmailNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    //id로 특정 회원을 조회 할 떄 찾을 수 없는 예외 처리
     @ExceptionHandler(MemberIdNotFoundException.class)
     public ResponseEntity<String> handleMemberIdNotFoundException(MemberIdNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    //상태를 추가할 때 중복
+    @ExceptionHandler(DuplicateMemberStateException.class)
+    public ResponseEntity<String> handleDuplicateMemberStateException(DuplicateMemberStateException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 }
