1f7c68bbce4c7aff936e1b56c9006bd21c884d35
yunjiho <yoonwlgh12@gmail.com>
2024-12-16 12:47:07 +0900

    전체 회원 조회하는 controller 테스트 코드 작성했습니다.

--- src/test/java/com/nhnacademy/book/member/domain/controller/MemberControllerTest.java
+++ src/test/java/com/nhnacademy/book/member/domain/controller/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.book.member.domain.controller;//package com.nhnacademy.book.member.domain.controller;
 
+import com.nhnacademy.book.member.domain.Member;
 import com.nhnacademy.book.member.domain.MemberGrade;
 import com.nhnacademy.book.member.domain.MemberStatus;
 import com.nhnacademy.book.member.domain.dto.*;
@@ -11,9 +12,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -190,5 +195,36 @@ class MemberControllerTest {
         verify(memberService, times(1)).withdrawMember(memberId);
     }
 
+    @Test
+    void getMembers() {
+        MemberSearchRequestDto memberSearchRequestDto = new MemberSearchRequestDto();
+        memberSearchRequestDto.setPage(0);
+        memberSearchRequestDto.setSize(10);
+
+        MemberGrade memberGrade = new MemberGrade();
+        memberGrade.setMemberGradeName("NORMAL");
+
+        MemberStatus memberStatus = new MemberStatus();
+        memberStatus.setMemberStateName("ACTIVE");
+
+
+        MemberSearchResponseDto response1 = new MemberSearchResponseDto("윤지호", "010-7237-3951", "yoonwlgh12@naver.com", LocalDate.of(2000,3,9), memberGrade.getMemberGradeName(), memberStatus.getMemberStateName());
+        MemberSearchResponseDto response2 = new MemberSearchResponseDto("윤지호2", "010-7237-3952", "yoonwlgh123@naver.com", LocalDate.of(2001,3,9), memberGrade.getMemberGradeName(), memberStatus.getMemberStateName());
+
+        Page<MemberSearchResponseDto> page = new PageImpl<>(Arrays.asList(response1, response2));
+
+        when(memberService.getMembers(memberSearchRequestDto)).thenReturn(page);
+
+        ResponseEntity<Page<MemberSearchResponseDto>> responseEntity = memberController.getMembers(memberSearchRequestDto);
+
+        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertNotNull(responseEntity.getBody());
+        assertEquals(2, responseEntity.getBody().getContent().size());
+        assertEquals("윤지호", responseEntity.getBody().getContent().get(0).getName());
+        assertEquals("윤지호2", responseEntity.getBody().getContent().get(1).getName());
+
+
+
+    }
 }
 
