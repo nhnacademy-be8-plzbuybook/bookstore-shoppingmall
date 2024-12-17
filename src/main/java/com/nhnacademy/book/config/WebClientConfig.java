b7648f229865f94aed56cbbe9e5486c902b229d4
choegwanghyeog <fkqlaus@naver.com>
2024-12-17 17:49:50 +0900

    book 수정

--- src/main/java/com/nhnacademy/book/config/WebClientConfig.java
+++ src/main/java/com/nhnacademy/book/config/WebClientConfig.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
 @Configuration
 @Slf4j
+@Profile("!test") // 'test' 프로파일이 아닐 때만 실행
 public class WebClientConfig {
 
     @Bean
