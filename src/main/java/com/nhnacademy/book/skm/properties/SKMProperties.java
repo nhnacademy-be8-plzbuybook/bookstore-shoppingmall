f0c9e480aae2c359e95b68bf5464da0ba92786b7
yunjiho <yoonwlgh12@gmail.com>
2024-12-19 10:43:20 +0900

    yml에서 비밀키 id를 저장해두고 읽어오는 properties를 만들었습니다.

---
+++ src/main/java/com/nhnacademy/book/skm/properties/SKMProperties.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.book.skm.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "app.skm")
+public class SKMProperties {
+    private String url;
+    private String appKey;
+    private String keystoreFile;
+    private String password;
+    private Database Database;
+
+    @Getter
+    @Setter
+    public static class Database {
+        private String url;
+        private String username;
+        private String password;
+    }
+}
