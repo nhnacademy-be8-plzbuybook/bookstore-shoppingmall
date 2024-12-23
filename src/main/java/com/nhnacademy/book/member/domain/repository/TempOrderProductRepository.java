dbb87030f373095a0add86c9c3cc50cd57082148
kkkkimtaehyeon <kth1927@naver.com>
2024-12-23 18:02:43 +0900

    cors 설정 삭제

--- src/main/java/com/nhnacademy/book/member/domain/repository/TempOrderProductRepository.java
+++ src/main/java/com/nhnacademy/book/member/domain/repository/TempOrderProductRepository.java
@@ -3,5 +3,5 @@ package com.nhnacademy.book.member.domain.repository;
 import com.nhnacademy.book.member.domain.OrderProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+public interface TempOrderProductRepository extends JpaRepository<OrderProduct, Long> {
 }
