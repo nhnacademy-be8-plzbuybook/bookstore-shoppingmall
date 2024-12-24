580d0d3141dbbad587d395cad61c582315a8d92c
choegwanghyeog <fkqlaus@naver.com>
2024-12-24 17:20:26 +0900

    엘라스틱 서치 서비스 ,컨트롤러 추가 진행중

---
+++ src/main/java/com/nhnacademy/book/book/elastic/repository/AuthorSearchRepository.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.book.book.elastic.repository;
+
+import com.nhnacademy.book.book.elastic.document.AuthorDocument;
+import com.nhnacademy.book.book.entity.Author;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorSearchRepository extends ElasticsearchRepository<AuthorDocument, Long> {
+
+    AuthorDocument findByAuthorName(String authorName);
+
+}
