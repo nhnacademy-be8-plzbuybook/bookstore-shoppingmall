e991b99497bc4adda31afb03c2d9e30a0ea57178
kkkkimtaehyeon <kth1927@naver.com>
2024-12-24 09:49:08 +0900

    회원도메인의 orderProduct temp 붙임

--- src/main/java/com/nhnacademy/book/member/domain/repository/TempOrderProductRepository.java
+++ src/main/java/com/nhnacademy/book/member/domain/repository/TempOrderProductRepository.java
@@ -1,7 +1,7 @@
 package com.nhnacademy.book.member.domain.repository;
 
-import com.nhnacademy.book.member.domain.OrderProduct;
+import com.nhnacademy.book.member.domain.TempOrderProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TempOrderProductRepository extends JpaRepository<OrderProduct, Long> {
+public interface TempOrderProductRepository extends JpaRepository<TempOrderProduct, Long> {
 }
