da576bb088b4dc55fceb581827fe0d848f8b1e88
sugwan <skhrnt2945@gmail.com>
2024-12-24 18:45:24 +0900

    fix: 엘라스틱 서치 설정 분리

--- src/main/java/com/nhnacademy/book/config/ElasticsearchConfig.java
+++ src/main/java/com/nhnacademy/book/config/ElasticsearchConfig.java
@@ -3,8 +3,23 @@ package com.nhnacademy.book.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(basePackages = {
+        "com.nhnacademy.book.book.repository",
+        "com.nhnacademy.book.member.domain.repository",
+        "com.nhnacademy.book.wrappingPaper.repository",
+        "com.nhnacademy.book.point.repository",
+        "com.nhnacademy.book.deliveryFeePolicy.repository",
+        "com.nhnacademy.book.order.repository",
+        "com.nhnacademy.book.orderProduct.repository",
+        "com.nhnacademy.book.payment.repository",
+        "com.nhnacademy.book.review.repository"
+
+})
+@EnableElasticsearchRepositories("com.nhnacademy.book.book.elastic.repository")
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
     @Override
