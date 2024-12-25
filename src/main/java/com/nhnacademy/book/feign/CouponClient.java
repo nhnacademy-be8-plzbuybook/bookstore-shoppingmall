43fcf57e38491cca3fb234c2ccd8dfecbe72f3bb
sugwan <skhrnt2945@gmail.com>
2024-12-25 16:28:52 +0900

    MemberServiceImpl 및 테스트 코드 수정 : Welcome 쿠폰 생성 호출을 메서드 내부에서 분리하였습니다

--- src/main/java/com/nhnacademy/book/feign/CouponClient.java
+++ src/main/java/com/nhnacademy/book/feign/CouponClient.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "bookstore-coupon-dev")
 public interface CouponClient {
 
+    // Welcome 쿠폰 발급 요청 -> 쿠폰서버의 컨트롤러 호출
     @PostMapping("/api/coupons/welcome-coupon")
     void issueWelcomeCoupon(@RequestBody WelComeCouponRequestDto requestDto);
 }
