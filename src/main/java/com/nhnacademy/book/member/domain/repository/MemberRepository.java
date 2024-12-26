a7ca5aaa0178776500082e6dc686fe73bd41e058
yunjiho <yoonwlgh12@gmail.com>
2024-12-26 15:22:04 +0900

    email을 통해 회원의 상태와 등급을 구해오는 repository를 구현했습니다.

--- src/main/java/com/nhnacademy/book/member/domain/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/book/member/domain/repository/MemberRepository.java
@@ -1,13 +1,17 @@
 package com.nhnacademy.book.member.domain.repository;
 
 import com.nhnacademy.book.member.domain.Member;
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     Optional<Member> findByEmail(String email);
+    @Query("SELECT m FROM Member m JOIN FETCH m.memberGrade JOIN FETCH m.memberStatus WHERE m.email = :email")
+    Optional<Member> findByEmailWithGradeAndStatus(@Param("email") String email);
 }
  
 
