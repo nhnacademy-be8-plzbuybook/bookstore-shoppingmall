3ccf147bbd8aa339d71d004039dd325ee8dcc61b
sugwan <skhrnt2945@gmail.com>
2024-12-27 11:00:01 +0900

    페인클라이언트 생일쿠폰 요청 추가

--- src/main/java/com/nhnacademy/book/feign/CouponClient.java
+++ src/main/java/com/nhnacademy/book/feign/CouponClient.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.book.feign;
 
+import com.nhnacademy.book.feign.dto.BirthdayCouponRequestDto;
 import com.nhnacademy.book.feign.dto.WelComeCouponRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,5 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface CouponClient {
 
     @PostMapping("/api/coupons/welcome")
-    void issueWelcomeCoupon(@RequestBody WelComeCouponRequestDto requestDto);
+    ResponseEntity<String> issueWelcomeCoupon(@RequestBody WelComeCouponRequestDto requestDto);
+
+    @PostMapping("/coupons/birthday")
+    ResponseEntity<String> issueBirthdayCoupon(@RequestBody BirthdayCouponRequestDto requestDto);
 }
