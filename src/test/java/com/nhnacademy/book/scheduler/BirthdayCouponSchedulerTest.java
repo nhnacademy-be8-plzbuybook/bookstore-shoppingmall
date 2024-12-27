fa9fba6741f0d55456543500ccf3cde6ca661f73
sugwan <skhrnt2945@gmail.com>
2024-12-27 15:33:43 +0900

    생일쿠폰 스케줄러 테스트 - 자동스케줄러가 되는지 확인

--- src/test/java/com/nhnacademy/book/scheduler/BirthdayCouponSchedulerTest.java
+++ src/test/java/com/nhnacademy/book/scheduler/BirthdayCouponSchedulerTest.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.book.scheduler;
 
 import com.nhnacademy.book.member.domain.service.BirthdayCouponService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,9 +13,9 @@ import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class BirthdayCouponSchedulerTest {
@@ -33,6 +32,19 @@ class BirthdayCouponSchedulerTest {
         int month = LocalDate.now().getMonthValue();
         Pageable expectedPageable = PageRequest.of(0, 100, Sort.by("memberId"));
         birthdayCouponScheduler.birthdayCoupon();
+
         verify(birthdayCouponService, times(1)).issueBirthdayCoupons(eq(month), eq(expectedPageable));
+    }
+
+    @DisplayName("특정 달에 스케줄러가 작동하는지 테스트")
+    @Test
+    void testScheduledTrigger() {
+        LocalDate date = LocalDate.of(2024, 12, 1);
+        int month = date.getMonthValue();
+        Pageable pageable = PageRequest.of(0, 100, Sort.by("memberId"));
+
+        birthdayCouponScheduler.birthdayCoupon();
+
+        verify(birthdayCouponService, times(1)).issueBirthdayCoupons(eq(month), eq(pageable));
     }
 }
