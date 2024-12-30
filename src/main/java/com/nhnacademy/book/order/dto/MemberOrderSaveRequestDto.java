1bae1bfdddd555ec9025a693124ff2d53202b8f3
kkkkimtaehyeon <kth1927@naver.com>
2024-12-30 16:51:18 +0900

    회원, 비회원 주문 저장 기능 구현

---
+++ src/main/java/com/nhnacademy/book/order/dto/MemberOrderSaveRequestDto.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.book.order.dto;
+
+import lombok.Getter;
+
+
+@Getter
+public class MemberOrderSaveRequestDto {
+    private Long memberId; //TODO: 둘중에 하나만 해야됨
+    private String memberEmail;
+    private String orderId;
+
+    public MemberOrderSaveRequestDto(String memberEmail, String orderId) {
+        this.memberEmail = memberEmail;
+        this.orderId = orderId;
+    }
+
+    public MemberOrderSaveRequestDto(Long memberId, String orderId) {
+        this.memberId = memberId;
+        this.orderId = orderId;
+    }
+}
