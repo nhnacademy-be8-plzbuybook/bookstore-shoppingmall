5177390044dcdd3400ca0982f9991c1a37150c31
kkkkimtaehyeon <kth1927@naver.com>
2024-12-30 13:50:26 +0900

    주문 캐싱 서비스 구현

---
+++ src/main/java/com/nhnacademy/book/order/service/impl/OrderCacheServiceImpl.java
@@ -0,0 +1,64 @@
+package com.nhnacademy.book.order.service.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.book.order.dto.orderRequests.OrderRequestDto;
+import com.nhnacademy.book.order.service.OrderCacheService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
+@Service
+public class OrderCacheServiceImpl implements OrderCacheService {
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 주문 캐시데이터 저장
+     *
+     * @param orderId 주문 ID
+     * @param order 주문요청 DTO
+     */
+    @Override
+    public void saveOrderCache(String orderId, OrderRequestDto order) {
+        String key = getOrderCacheKey(orderId);
+        try {
+            String jsonString = objectMapper.writeValueAsString(order);
+            redisTemplate.opsForValue().set(key, jsonString, 15L, TimeUnit.MINUTES);  // 15분간 주문정보 캐시 유지
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("주문정보 변환 오류가 발생했습니다.", e);
+        } catch (Exception e) {
+            throw new RuntimeException("주문정보 캐싱 중 오류가 발생했습니다.", e);
+        }
+    }
+
+    /**
+     * 주문 캐시데이터 조회
+     *
+     * @param orderId 주문 ID
+     * @return 주문요청 DTO
+     */
+    @Override
+    public OrderRequestDto fetchOrderCache(String orderId) {
+        String key = getOrderCacheKey(orderId);
+        Object value = redisTemplate.opsForValue().getAndDelete(key);
+
+        if (value == null) {
+            throw new IllegalArgumentException("15분이 지나 주문정보가 만료되었습니다.");
+        }
+        try {
+            return objectMapper.readValue((String) value, OrderRequestDto.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("주문정보 변환 오류가 발생했습니다.", e);
+        }
+    }
+
+    private String getOrderCacheKey(String orderId) {
+        return "order:" + orderId;
+    }
+}
