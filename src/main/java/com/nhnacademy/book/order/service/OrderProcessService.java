972eb80f73ff85bb420f87d02c3273879b264a4f
kkkkimtaehyeon <kth1927@naver.com>
2024-12-30 14:25:05 +0900

    메소드명 수정

--- src/main/java/com/nhnacademy/book/order/service/OrderProcessService.java
+++ src/main/java/com/nhnacademy/book/order/service/OrderProcessService.java
@@ -4,6 +4,6 @@ import com.nhnacademy.book.order.dto.orderRequests.OrderRequestDto;
 import com.nhnacademy.book.order.dto.orderResponse.OrderResponseDto;
 
 public interface OrderProcessService {
-    <T extends OrderRequestDto> OrderResponseDto requestOrder(T orderRequest);
+    <T extends OrderRequestDto> OrderResponseDto processRequestedOrder(T orderRequest);
     String completeOrder(String orderId);
 }
