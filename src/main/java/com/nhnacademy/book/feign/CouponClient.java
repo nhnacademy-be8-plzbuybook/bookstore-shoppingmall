189bd2a2654f4186f2f16b7ed7ed49e4610998dc
sugwan <skhrnt2945@gmail.com>
2025-01-03 18:04:02 +0900

    페인클라이언트 - 쿠폰서버 수정

--- src/main/java/com/nhnacademy/book/feign/CouponClient.java
+++ src/main/java/com/nhnacademy/book/feign/CouponClient.java
@@ -3,6 +3,7 @@ package com.nhnacademy.book.feign;
 import com.nhnacademy.book.feign.dto.BirthdayCouponRequestDto;
 import com.nhnacademy.book.feign.dto.MemberCouponResponseDto;
 import com.nhnacademy.book.feign.dto.WelComeCouponRequestDto;
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,21 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "coupon")
 public interface CouponClient {
 
+    // Welcome 쿠폰
     @PostMapping("/api/coupons/welcome")
-    ResponseEntity<String> issueWelcomeCoupon(@RequestBody WelComeCouponRequestDto requestDto);
+    ResponseEntity<String> issueWelcomeCoupon(@RequestBody @Valid WelComeCouponRequestDto requestDto);
 
-    @PostMapping("/coupons/birthday")
+    // 생일 쿠폰
+    @PostMapping("/api/coupons/birthday")
     ResponseEntity<String> issueBirthdayCoupon(@RequestBody BirthdayCouponRequestDto requestDto);
 
-    // 회원 고유 ID 로 회원이 보유한 쿠폰 목록 조회
-    @GetMapping("/api/coupons/member-coupons/member/{memberId}")
-    ResponseEntity<Page<MemberCouponResponseDto>> getMemberCouponsByMemberId(
-            @PathVariable Long memberId,
-            @RequestParam int page,
-            @RequestParam int size
-    );
-
-    // 회원 고유 ID 로 회원 본인이 사용가능한 쿠폰 목록 조회
-    @GetMapping("/member-coupons/member/{memberId}/unused")
-    ResponseEntity<Page<MemberCouponResponseDto>> getUnusedMemberCouponsByMemberId(@PathVariable("memberId") Long memberId, Pageable pageable);
 }
