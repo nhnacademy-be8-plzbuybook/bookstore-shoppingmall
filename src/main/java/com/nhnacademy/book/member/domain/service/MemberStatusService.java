b8398f83c3a55c635215c6d0a93c61417b8141e3
yunjiho <yoonwlgh12@gmail.com>
2025-01-05 21:01:02 +0900

    모든 상태를 가져오는 service를 구현했습니다.

--- src/main/java/com/nhnacademy/book/member/domain/service/MemberStatusService.java
+++ src/main/java/com/nhnacademy/book/member/domain/service/MemberStatusService.java
@@ -3,7 +3,10 @@ package com.nhnacademy.book.member.domain.service;
 import com.nhnacademy.book.member.domain.MemberStatus;
 import com.nhnacademy.book.member.domain.dto.MemberStatusCreateRequestDto;
 
+import java.util.List;
+
 public interface MemberStatusService {
     MemberStatus createMemberStatus(MemberStatusCreateRequestDto memberStatusCreateRequestDto);
     void updateMemberStatusActiveByEmail(String email);
+    List<MemberStatus> getAllMemberStatuses();
 }
