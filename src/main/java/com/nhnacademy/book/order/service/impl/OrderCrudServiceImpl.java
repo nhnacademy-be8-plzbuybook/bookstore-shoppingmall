31cf8ce32340c2fef3caa73aa3c486baab381ffb
kkkkimtaehyeon <kth1927@naver.com>
2025-01-06 02:50:59 +0900

    배송비 속성 추가

--- src/main/java/com/nhnacademy/book/order/service/impl/OrderCrudServiceImpl.java
+++ src/main/java/com/nhnacademy/book/order/service/impl/OrderCrudServiceImpl.java
@@ -17,7 +17,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -38,6 +37,7 @@ public class OrderCrudServiceImpl implements OrderCrudService {
                 .id(generateOrderId())
                 .number(generateOrderNumber(currentTime))
                 .name(generateOrderName(orderRequest))
+                .deliveryFee(orderRequest.getDeliveryFee())
                 .usedPoint(orderRequest.getUsedPoint())
                 .deliveryWishDate(orderRequest.getDeliveryWishDate())
                 .orderedAt(currentTime)
@@ -50,6 +50,7 @@ public class OrderCrudServiceImpl implements OrderCrudService {
         BigDecimal paymentPrice = orderRequest.getOrderPrice().add(orderRequest.getDeliveryFee());
         return new OrderResponseDto(savedOrder.getId(), paymentPrice, savedOrder.getName());
     }
+
 
     /**
      * 주문이름 생성
@@ -65,14 +66,6 @@ public class OrderCrudServiceImpl implements OrderCrudService {
         }
         return book.getBookTitle();
     }
-//    //TODO: 임시
-//    private String generateOrderName(OrderRequestDto order) {
-//        List<OrderProductRequestDto> orderProducts = order.getOrderProducts();
-//        if (orderProducts.size() > 1) {
-//            return String.format("%s 외 %d 건", "수학의 정석", orderProducts.size());
-//        }
-//        return "수학의 정석";
-//    }
 
 
     /**
@@ -83,11 +76,9 @@ public class OrderCrudServiceImpl implements OrderCrudService {
      */
     private String generateOrderNumber(LocalDateTime orderedAt) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd-HHmmssSSSS");
-        String orderedAtString = orderedAt.format(formatter);
-        int randomValue = new Random().nextInt(9999); // 0~9999 랜덤 값
-
-        return orderedAtString + "-" + String.format("%04d", randomValue);
+        return orderedAt.format(formatter);
     }
+
 
     /**
      * UUID를 기반으로 주문 ID 생성
@@ -97,34 +88,4 @@ public class OrderCrudServiceImpl implements OrderCrudService {
     private String generateOrderId() {
         return UUID.randomUUID().toString();
     }
-
-//
-//    private BigDecimal calculateOrderPrice(OrderRequestDto order) {
-//        BigDecimal orderPrice = BigDecimal.ZERO;
-//
-//        for (OrderProductRequestDto orderProduct : order.getOrderProducts()) {
-//            // 주문상품 가격 계산
-//            orderPrice = orderPrice.add(orderProduct.getPrice().multiply(BigDecimal.valueOf(orderProduct.getQuantity())));
-//            // 주문상품 포장지 가격 계산
-//            if (orderProduct.getWrapping() != null) {
-//                orderPrice = orderPrice.add(orderProduct.getWrapping().getPrice().multiply(BigDecimal.valueOf(orderProduct.getWrapping().getQuantity())));
-//            }
-//        }
-//        return orderPrice;
-//    }
-//
-//
-//    private BigDecimal calculateCouponDiscount(OrderRequestDto order) {
-//        BigDecimal couponDiscount = BigDecimal.ZERO;
-//
-//        for (OrderProductRequestDto orderProduct : order.getOrderProducts()) {
-//            // 주문상품 적용 쿠폰 계산
-//            if (orderProduct.getAppliedCoupons() != null && !orderProduct.getAppliedCoupons().isEmpty()) {
-//                for (OrderProductAppliedCouponDto coupon : orderProduct.getAppliedCoupons()) {
-//                    couponDiscount = couponDiscount.add(coupon.getDiscount());
-//                }
-//            }
-//        }
-//        return couponDiscount;
-//    }
 }
