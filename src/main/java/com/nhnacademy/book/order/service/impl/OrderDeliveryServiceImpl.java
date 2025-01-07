61e0308c02a41ad3002e7479be176d8a3f5fd2b7
kkkkimtaehyeon <kth1927@naver.com>
2025-01-07 16:50:04 +0900

    주문 배송 진행 중입니다.

---
+++ src/main/java/com/nhnacademy/book/order/service/impl/OrderDeliveryServiceImpl.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.book.order.service.impl;
+
+import com.nhnacademy.book.deliveryFeePolicy.exception.NotFoundException;
+import com.nhnacademy.book.order.dto.OrderDeliveryRegisterRequestDto;
+import com.nhnacademy.book.order.entity.Orders;
+import com.nhnacademy.book.order.repository.OrderDeliveryRepository;
+import com.nhnacademy.book.order.repository.OrderRepository;
+import com.nhnacademy.book.order.service.OrderDeliveryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OrderDeliveryServiceImpl implements OrderDeliveryService {
+    private final OrderDeliveryRepository orderDeliveryRepository;
+    private final OrderRepository orderRepository;
+
+    @Override
+    public String registerOrderDelivery(OrderDeliveryRegisterRequestDto registerRequest) {
+        Orders order = orderRepository.findById(registerRequest.getOrderId()).orElseThrow(() -> new NotFoundException("찾을 수 없는 주문입니다."));
+
+        return "";
+    }
+}
