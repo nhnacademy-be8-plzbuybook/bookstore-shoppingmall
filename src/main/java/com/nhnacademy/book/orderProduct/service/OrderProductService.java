dad497f7d33ae80132890b09de4f7f082bdb101d
yunjiho <yoonwlgh12@gmail.com>
2025-01-08 16:54:08 +0900

    리뷰 추가에 필요한 repository를 만들었습니다.

--- src/main/java/com/nhnacademy/book/orderProduct/service/OrderProductService.java
+++ src/main/java/com/nhnacademy/book/orderProduct/service/OrderProductService.java
@@ -4,8 +4,11 @@ import com.nhnacademy.book.order.dto.orderRequests.OrderProductRequestDto;
 import com.nhnacademy.book.order.entity.Orders;
 import com.nhnacademy.book.orderProduct.entity.OrderProduct;
 
+import java.util.Optional;
+
 public interface OrderProductService {
     OrderProduct saveOrderProduct(Orders order, OrderProductRequestDto orderProductRequest);
+    Optional<OrderProduct> findOrderProductBySellingBookId(Long sellingBookId);
     //TODO: 주문상품 상태변경
     //TODO: 주문상품 취소
     //TODO: 주문상품 반품요청
