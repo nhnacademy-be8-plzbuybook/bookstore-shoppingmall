c7ec8ed40c87770e872f72e37b67498348b90bdb
yunjiho <yoonwlgh12@gmail.com>
2025-01-08 16:54:21 +0900

    리뷰 추가에 필요한 repository를 만들었습니다.

--- src/main/java/com/nhnacademy/book/review/repository/ReviewRepository.java
+++ src/main/java/com/nhnacademy/book/review/repository/ReviewRepository.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.book.review.repository;
 
+import com.nhnacademy.book.orderProduct.entity.OrderProduct;
 import com.nhnacademy.book.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    boolean existsByOrderProduct(OrderProduct orderProduct);
 }
