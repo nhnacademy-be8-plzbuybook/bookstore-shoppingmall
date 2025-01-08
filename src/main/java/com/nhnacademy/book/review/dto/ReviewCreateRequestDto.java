7f3aff59eba0207db0ce56186db6dded16b5c982
yunjiho <yoonwlgh12@gmail.com>
2025-01-08 16:55:20 +0900

    객체 관련 설정했습니다.

--- src/main/java/com/nhnacademy/book/review/dto/ReviewCreateRequestDto.java
+++ src/main/java/com/nhnacademy/book/review/dto/ReviewCreateRequestDto.java
@@ -1,15 +1,24 @@
 package com.nhnacademy.book.review.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewCreateRequestDto {
+
+    private Long memberId;
+
+    private Long orderProductId;
+
     private Integer score;
+
     private String content;
 }
