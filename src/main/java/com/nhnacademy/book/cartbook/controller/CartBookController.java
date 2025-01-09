82fae7ed4a0a396cd5f14b8eb5349356309c14d4
jalyice <jalyice@gmail.com>
2025-01-09 15:40:01 +0900

    feat: 회원 장바구니 리팩토링
    - 불필요한 함수 제거
    - 비회원 장바구니 호출 부분 제거
    - DTO 통합

--- src/main/java/com/nhnacademy/book/cartbook/controller/CartBookController.java
+++ src/main/java/com/nhnacademy/book/cartbook/controller/CartBookController.java
@@ -2,34 +2,21 @@ package com.nhnacademy.book.cartbook.controller;
 
 import com.nhnacademy.book.cartbook.dto.request.*;
 import com.nhnacademy.book.cartbook.dto.response.*;
-import com.nhnacademy.book.cartbook.service.CartBookGuestService;
 import com.nhnacademy.book.cartbook.service.CartBookMemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/bookstore/carts")
 public class CartBookController {
     private final CartBookMemberService cartBookMemberService;
-    private final CartBookGuestService cartBookGuestService;
 
-    public CartBookController(CartBookMemberService cartBookMemberService, CartBookGuestService cartBookGuestService) {
+    public CartBookController(CartBookMemberService cartBookMemberService) {
         this.cartBookMemberService = cartBookMemberService;
-        this.cartBookGuestService = cartBookGuestService;
     }
 
-
-    @GetMapping("/{cartBookId}")
-    public ResponseEntity<List<ReadCartBookResponseDto>> getBookCart(
-            @PathVariable Long cartBookId) {
-            //@RequestHeader(value = "X-USER-ID", required = false) String email
-
-        List<ReadCartBookResponseDto> responses = cartBookGuestService.readAllCartBook(cartBookId);
-        return ResponseEntity.ok(responses);
-    }
 
     @GetMapping()
     public ResponseEntity<List<ReadCartBookResponseDto>> getAllCartBooks
@@ -42,20 +29,13 @@ public class CartBookController {
     public ResponseEntity<Long> createBookCart(
             @RequestBody CreateCartBookRequestDto createCartBookRequestDto,
             @RequestHeader(value = "X-USER-ID", required = false) String email) {
-        if(Objects.isNull(email)) {
-            Long cartBookId = cartBookGuestService.createCartBook(createCartBookRequestDto.sellingBookId(),
-                    null,
-                    createCartBookRequestDto.quantity()
-                    );
-            return ResponseEntity.ok(cartBookId);
-        } else {
-            createCartBookRequestDto = CreateCartBookRequestDto.builder()
+
+        createCartBookRequestDto = CreateCartBookRequestDto.builder()
                     .sellingBookId(createCartBookRequestDto.sellingBookId())
                     .quantity(createCartBookRequestDto.quantity())
                     .build();
-            return ResponseEntity.ok(cartBookMemberService.createBookCartMember(createCartBookRequestDto,email));
-        }
 
+        return ResponseEntity.ok(cartBookMemberService.createBookCartMember(createCartBookRequestDto, email));
 
     }
 
@@ -63,16 +43,8 @@ public class CartBookController {
     public ResponseEntity<Long> updateBookCart(
             @RequestBody UpdateCartBookRequestDto updateCartBookRequestDto,
             @RequestHeader(value = "X-USER-ID",required = false) String email) {
-        if(Objects.isNull(email)) {
-            cartBookGuestService.updateCartBook(
-                    updateCartBookRequestDto.cartBookId(),
-                    updateCartBookRequestDto.sellingBookId(),
-                    updateCartBookRequestDto.quantity()
-            );
-            return ResponseEntity.ok(updateCartBookRequestDto.cartBookId());
-        } else {
-            return ResponseEntity.ok(cartBookMemberService.updateBookCartMember(updateCartBookRequestDto, email));
-        }
+
+        return ResponseEntity.ok(cartBookMemberService.updateBookCartMember(updateCartBookRequestDto, email));
 
     }
 
@@ -81,26 +53,13 @@ public class CartBookController {
             @RequestBody DeleteCartBookRequestDto deleteCartBookRequestDto,
             @RequestHeader(value = "X-USER-ID",required = false) String email) {
 
-        if (Objects.isNull(email)) {
-            cartBookGuestService.deleteCartBook(
-                    deleteCartBookRequestDto.cartBookId(),
-                    deleteCartBookRequestDto.cartId()
-            );
-            return ResponseEntity.ok("delete success");
-        } else {
-            return ResponseEntity.ok(cartBookMemberService.deleteBookCartMember(deleteCartBookRequestDto.cartBookId(), email));
-        }
+        return ResponseEntity.ok(cartBookMemberService.deleteBookCartMember(deleteCartBookRequestDto.cartBookId(), email));
     }
 
 
     @DeleteMapping("/{cartId}")
     public ResponseEntity<String> deleteAllBooks(
-            @PathVariable(required = false) Long cartId,
             @RequestHeader(value = "X-USER-ID",required = false) String email) {
-        if(Objects.isNull(email)) {
-            String response = cartBookGuestService.deleteAllGuestBookCart(cartId);
-            return ResponseEntity.ok(response);
-        }
 
         return ResponseEntity.ok(cartBookMemberService.deleteAllBookCartMember(email));
     }
