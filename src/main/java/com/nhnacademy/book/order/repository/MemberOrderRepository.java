2d385bc20773d79e698ee3e50f1796502fad32f4
yunjiho <yoonwlgh12@gmail.com>
2025-01-10 11:11:41 +0900

    save

--- src/main/java/com/nhnacademy/book/order/repository/MemberOrderRepository.java
+++ src/main/java/com/nhnacademy/book/order/repository/MemberOrderRepository.java
@@ -3,8 +3,9 @@ package com.nhnacademy.book.order.repository;
 import com.nhnacademy.book.order.entity.MemberOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberOrderRepository extends JpaRepository<MemberOrder, Long> {
-    Optional<MemberOrder> findByOrder_IdAndMember_memberId(String orderId, Long memberId);
+    List<MemberOrder> findByOrder_IdAndMember_memberId(String orderId, Long memberId);
 }
