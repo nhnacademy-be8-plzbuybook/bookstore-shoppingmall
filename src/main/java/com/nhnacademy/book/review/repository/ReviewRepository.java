6487429f8fd2eea763cda23c42d46d86472ee443
yunjiho <yoonwlgh12@gmail.com>
2025-01-12 18:18:21 +0900

    책 id를 통해 리뷰를 가져오는 것을 repository에 추가했습니다.

--- src/main/java/com/nhnacademy/book/review/repository/ReviewRepository.java
+++ src/main/java/com/nhnacademy/book/review/repository/ReviewRepository.java
@@ -3,7 +3,14 @@ package com.nhnacademy.book.review.repository;
 import com.nhnacademy.book.orderProduct.entity.OrderProduct;
 import com.nhnacademy.book.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     boolean existsByOrderProduct(OrderProduct orderProduct);
+
+    //책 id를 통해 해당하는 리뷰 불러오기
+    @Query("SELECT r FROM Review r WHERE r.orderProduct.sellingBook.sellingBookId = :bookId")
+    List<Review> findReviewByBookId(Long bookId);
 }
