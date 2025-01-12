0bc110783e40451cfb179ac070a2f5b01500f668
yunjiho <yoonwlgh12@gmail.com>
2025-01-12 18:18:41 +0900

    필요 없는 service를 지우고 구현에 필요 한 service만 남겼습니다.

--- src/main/java/com/nhnacademy/book/review/service/ReviewService.java
+++ src/main/java/com/nhnacademy/book/review/service/ReviewService.java
@@ -3,30 +3,20 @@ package com.nhnacademy.book.review.service;
 import com.nhnacademy.book.review.dto.ReviewCreateRequestDto;
 import com.nhnacademy.book.review.dto.ReviewResponseDto;
 import com.nhnacademy.book.review.dto.ReviewUpdateRequestDto;
+import com.nhnacademy.book.review.dto.ReviewWithReviewImageDto;
 
 import java.util.List;
 
 public interface ReviewService {
 
-    //특정 회원이 리뷰 작성(productId = order_product_id)
+    //특정 회원이 리뷰 작성
     ReviewResponseDto createReview(ReviewCreateRequestDto createRequestDto, List<String> imageUrls);
 
-    //특정 상품에 대한 모든 리뷰 조회
-    List<ReviewResponseDto> getReviewsByProductId(Long productId);
+    //책에 대한 리뷰 조회
+    List<ReviewWithReviewImageDto> getReviewsWithReviewImagesByBookId(Long BookId);
 
-    //특정 회원이 작성한 리뷰 조회
-    List<ReviewResponseDto> getReviewsByMemberId(Long memberId);
+    //별점 평균
+    Double averageRatingByBookId(Long BookId);
 
-    //특정 리뷰 조회
-    ReviewResponseDto getReviewById(Long reviewId);
-
-    //리뷰 수정
-    ReviewResponseDto updateReview(Long reviewId, ReviewUpdateRequestDto reviewUpdateRequestDto);
-
-    //리뷰 평점 계산
-    Double calculateAverageScoreByProductId(Long productId);
-
-    //전체 리뷰 조회
-    List<ReviewResponseDto> getAllReviews();
 
 }
