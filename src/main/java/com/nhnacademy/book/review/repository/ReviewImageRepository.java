af071a7bcc38d753a39534a501631b2e3e02f1e4
yunjiho <yoonwlgh12@gmail.com>
2025-01-13 13:20:27 +0900

    리뷰 페이징 처리에 필요한 내용을 repository에 추가했습니다.

--- src/main/java/com/nhnacademy/book/review/repository/ReviewImageRepository.java
+++ src/main/java/com/nhnacademy/book/review/repository/ReviewImageRepository.java
@@ -10,4 +10,7 @@ public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> 
     //리뷰 아이디를 통해 리뷰 이미지를 가져오기
     @Query("SELECT ri FROM ReviewImage ri WHERE ri.review.orderProduct.sellingBook.sellingBookId = :bookId")
     List<ReviewImage> findReviewImageByBookId(Long bookId);
+
+    @Query("SELECT ri FROM ReviewImage ri WHERE ri.review.orderProduct.sellingBook.sellingBookId = :bookId AND ri.review.reviewId IN :reviewIds")
+    List<ReviewImage> findImagesByBookIdAndReviewIds(Long bookId, List<Long> reviewIds);
 }
