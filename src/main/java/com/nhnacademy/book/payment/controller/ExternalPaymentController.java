90e416e341387a989291fe7a705fa78961b3b0ca
kkkkimtaehyeon <kth1927@naver.com>
2025-01-18 15:43:16 +0900

    불필요한 예외전파 제거

--- src/main/java/com/nhnacademy/book/payment/controller/ExternalPaymentController.java
+++ src/main/java/com/nhnacademy/book/payment/controller/ExternalPaymentController.java
@@ -20,7 +20,7 @@ public class ExternalPaymentController {
 
 
     @PostMapping("/confirm/widget")
-    public ResponseEntity<JSONObject> confirmPayment(@RequestBody PaymentConfirmRequestDto confirmRequest) throws JsonProcessingException {
+    public ResponseEntity<JSONObject> confirmPayment(@RequestBody PaymentConfirmRequestDto confirmRequest) {
         // 결제 요청 전 저장한 정보와 같은지 검증
         paymentService.verifyPayment(confirmRequest);
         JSONObject response = paymentService.confirmPayment(confirmRequest);
