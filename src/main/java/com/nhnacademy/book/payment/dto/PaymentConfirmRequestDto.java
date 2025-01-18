5c3fbf67426075e8ee5e4b61bb44de49719ebf3a
kkkkimtaehyeon <kth1927@naver.com>
2025-01-18 15:42:11 +0900

    dto 검증조건 추가

--- src/main/java/com/nhnacademy/book/payment/dto/PaymentConfirmRequestDto.java
+++ src/main/java/com/nhnacademy/book/payment/dto/PaymentConfirmRequestDto.java
@@ -1,5 +1,8 @@
 package com.nhnacademy.book.payment.dto;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,8 +11,13 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Getter
 public class PaymentConfirmRequestDto {
+    @NotBlank
     private String paymentKey;
+    @NotBlank
     private String orderId;
+    @NotBlank
     private BigDecimal amount;
+    @Nullable
+    @Min(0)
     private Integer usedPoint;
 }
