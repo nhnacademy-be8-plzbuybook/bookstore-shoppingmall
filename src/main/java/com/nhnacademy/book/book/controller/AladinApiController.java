3c390100c06255dd01728df19872fad901196afa
Gksrufdk09 <rufdk09@naver.com>
2025-01-19 15:56:01 +0900

    알라딘 판매책 필드 수

--- src/main/java/com/nhnacademy/book/book/controller/AladinApiController.java
+++ src/main/java/com/nhnacademy/book/book/controller/AladinApiController.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.book.book.controller;
 
-//import com.nhnacademy.book.book.service.AladinApiService;
 import com.nhnacademy.book.book.service.api.ApiService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,16 +45,4 @@ public class AladinApiController {
     }
 
 
-    /**
-     * 특정 ISBN(ItemId)를 기준으로 데이터베이스에 저장
-     */
-    @PostMapping("/sync/itemid/{isbn13}")
-    public ResponseEntity<String> syncBookByIsbn(@PathVariable String isbn13) {
-        boolean isSaved = aladinApiService.saveBooksByIsbns(List.of(isbn13));
-        if (isSaved) {
-            return ResponseEntity.ok("Book with ISBN " + isbn13 + " has been successfully saved.");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No valid data found for ISBN " + isbn13 + ".");
-        }
-    }
 }
--- src/main/java/com/nhnacademy/book/book/dto/response/aladin/AladinBookListResponse.java
+++ src/main/java/com/nhnacademy/book/book/dto/response/aladin/AladinBookListResponse.java
@@ -1,52 +1,52 @@
-package com.nhnacademy.book.book.dto.response.aladin;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-
-/**
- * 리스트 API의 응답 전체 구조를 나타냄 (메타데이터와 도서 목록 포함).
- */
-@Getter
-@Setter
-public class AladinBookListResponse {
-    @JsonProperty("version")
-    private String version;
-
-    @JsonProperty("title")
-    private String title;
-
-    @JsonProperty("pubDate")
-    private String pubDate;
-
-    @JsonProperty("totalResults")
-    private int totalResults;
-
-    @JsonProperty("startIndex")
-    private int startIndex;
-
-    @JsonProperty("itemsPerPage")
-    private int itemsPerPage;
-
-    @JsonProperty("query")
-    private String query;
-
-    @JsonProperty("searchCategoryName")
-    private String searchCategoryName;
-
-    @JsonProperty("item")  // JSON에서 "item" 키에 해당
-    private List<AladinResponse> item;
-
-    public List<AladinResponse> getBooks() {
-        return item;
-    }
-
-    public void setBooks(List<AladinResponse> books) {
-        this.item = books;
-    }
-}
+//package com.nhnacademy.book.book.dto.response.aladin;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.util.List;
+//import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+//import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+//import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+//
+///**
+// * 리스트 API의 응답 전체 구조를 나타냄 (메타데이터와 도서 목록 포함).
+// */
+//@Getter
+//@Setter
+//public class AladinBookListResponse {
+//    @JsonProperty("version")
+//    private String version;
+//
+//    @JsonProperty("title")
+//    private String title;
+//
+//    @JsonProperty("pubDate")
+//    private String pubDate;
+//
+//    @JsonProperty("totalResults")
+//    private int totalResults;
+//
+//    @JsonProperty("startIndex")
+//    private int startIndex;
+//
+//    @JsonProperty("itemsPerPage")
+//    private int itemsPerPage;
+//
+//    @JsonProperty("query")
+//    private String query;
+//
+//    @JsonProperty("searchCategoryName")
+//    private String searchCategoryName;
+//
+//    @JsonProperty("item")  // JSON에서 "item" 키에 해당
+//    private List<AladinResponse> item;
+//
+//    public List<AladinResponse> getBooks() {
+//        return item;
+//    }
+//
+//    public void setBooks(List<AladinResponse> books) {
+//        this.item = books;
+//    }
+//}
