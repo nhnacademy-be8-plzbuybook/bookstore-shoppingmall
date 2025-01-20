b02df6c499c66037112c93393909c1b866859194
Gksrufdk09 <rufdk09@naver.com>
2025-01-20 17:21:29 +0900

    작가 추가 필드 ( 삭제는 안됨 )

--- src/main/java/com/nhnacademy/book/book/repository/AuthorRepository.java
+++ src/main/java/com/nhnacademy/book/book/repository/AuthorRepository.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.book.book.repository;
 
 import com.nhnacademy.book.book.entity.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -19,5 +21,7 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     @Modifying
     @Query("UPDATE Author a SET a.deletedAt = CURRENT_TIMESTAMP WHERE a.authorId = :authorId")
     void softDeleteById(Long authorId);
+
+    Page<Author> findByAuthorNameContaining(String authorName, Pageable pageable);
 
 }
