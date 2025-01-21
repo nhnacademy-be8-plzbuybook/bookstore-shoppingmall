8b9b38af6ca3ad53401b86e527a7df35ef0e1f4c
yunjiho <yoonwlgh12@gmail.com>
2025-01-21 09:15:53 +0900

    convertrer 테스트 코드 작성완료 했습니다

---
+++ src/test/java/com/nhnacademy/book/converter/OrderStatusConverterTest.java
@@ -0,0 +1,34 @@
+package com.nhnacademy.book.converter;
+
+import com.nhnacademy.book.order.enums.OrderStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderStatusConverterTest {
+
+    private final OrderStatusConverter converter = new OrderStatusConverter();
+
+    @Test
+    @DisplayName("OrderStatus -> Integer 변환 테스트")
+    void convertToDatabaseColumnTest() {
+        OrderStatus status = OrderStatus.SHIPPED;
+
+        Integer code = converter.convertToDatabaseColumn(status);
+
+        assertNotNull(code);
+        assertEquals(2, code);
+    }
+
+    @Test
+    @DisplayName("Integer -> OrderStatus 변환 테스트")
+    void convertToEntityAttributeTest() {
+        Integer code = 4;
+
+        OrderStatus status = converter.convertToEntityAttribute(code);
+
+        assertNotNull(status);
+        assertEquals(OrderStatus.DELIVERED, status); // 4는 DELIVERED에 매핑
+    }
+}
