a850c1b44f7963437da5a9217fa8d0f5a963eaa6
jalyice <jalyice@gmail.com>
2025-02-04 09:36:11 +0900

    feat: 개발환경과 배포환경 분리
    application-local.yml 은 개발환경
    application-nhn.yml 은 배포환경

--- src/main/java/com/nhnacademy/book/config/ElasticsearchConfig.java
+++ src/main/java/com/nhnacademy/book/config/ElasticsearchConfig.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.book.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -9,13 +10,23 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "com.nhnacademy.book.book.elastic.repository")
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
+    @Value("${spring.elasticsearch.uris}")
+    private String url;
+
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
 
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
 //                .connectedTo("localhost:9200") // 원격 서버 URL
-                .connectedTo("220.67.216.14:9200") // 원격 서버 URL
-                .withBasicAuth("elastic", "nhnacademy123!") // 기본 인증 추가
+//                .connectedTo("220.67.216.14:9200") // 원격 서버 URL
+//                .withBasicAuth("elastic", "nhnacademy123!") // 기본 인증 추가
+                .connectedTo(url) // 원격 서버 URL
+                .withBasicAuth(username, password) // 기본 인증 추가
                 .build();
     }
 
